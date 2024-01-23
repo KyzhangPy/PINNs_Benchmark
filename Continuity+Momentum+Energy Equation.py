b85676e399d41bdb2ecd4b042c5a5e904bde0f46
KyZhangS <142552994+KyzhangPy@users.noreply.github.com>
2024-01-23 18:28:29 +0800

    Update Continuity+Momentum+Energy Equation.py

--- Continuity+Momentum+Energy Equation.py
+++ Continuity+Momentum+Energy Equation.py
@@ -17,15 +17,60 @@ import matplotlib.pyplot as plt
 import scipy.io 
 ## 调用物理常量/单位库、常用的输入输出函数
 from scipy.interpolate import griddata 
-# 调用griddata差值函数（非规则网格的数据差值）
+## 调用griddata差值函数（非规则网格的数据差值）
 import time 
 ## 调用时间日期函数
 from itertools import product, combinations 
 ## itertools是用于迭代工具的标准库，itertools.product计算多个可迭代对象的笛卡尔积，itertools.combinations生成可迭代对象的所有长度为r的组合
 from mpl_toolkits.mplot3d import Axes3D
-## mpl_toolkits.mplot3d用于绘制和可视化三维图形，Axes3D是绘制3D图形的库
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-## 
+## mpl_toolkits是matplotlib的绘图工具包，mpl_toolkits.mplot3d用于绘制和可视化三维图形
 from plotting import newfig, savefig
+## plotting可以结合各种视觉元素和工具创建可视化图形的库
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+## axes_grid1是辅助类几何工具，可以用于显示多个图像
 import matplotlib.gridspec as gridspec
+## gridspec是专门指定画布中的子图位置的模块
+
+# 设置随机数种子
+np.random.seed(1234)
+tf.set_random_seed(1234)
+
+# 定义PINN的类（类是一种数据类型，代表着一类具有相同属性和方法的对象的集合）
+class PhysicsInformedNN:
+  #初始化类（定义类中的变量）
+  def __init__(self, x, y, t, u, v, layers):
+    
+    ## 数组的组合拼接，1表示按列拼接，X表示自变量的矩阵
+    X = np.concatenate([x, y, t], 1)
+    
+    ## 0表示返回矩阵中每一列的最小值，1表示返回每一行的最小值，即表示
+    self.lb = X.min(0)
+    self.ub = X.max(0)
+    ## 保存类的自变量矩阵
+    self.X = X
+    ## 分别保存类的自变量 X[：,0:1]取所有数据的第m到n-1列数据，即含左不含右
+    self.x = X[:,0:1]
+    self.y = X[:,1:2]
+    self.t = X[:,2:3]
+    ## 分别保存类的因变量
+    self.u = u
+    self.v = v
+    ## 保存类的NN层数
+    self.layers = layers
+    ## 初始化神经网络
+    self.weights, self.biases = self.initialize_NN(layers) 
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
