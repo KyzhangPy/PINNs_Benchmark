271755da37c857f0f15df313137a8d601a69f6ee
KyZhangS <142552994+KyzhangPy@users.noreply.github.com>
2024-01-23 11:35:03 +0800

    Update NS_Energy Equation.py

--- NS_Energy Equation.py
+++ NS_Energy Equation.py
@@ -1,8 +1,10 @@
 """
-@author: Maziar Raissi
+@author: Zhang Shuo
 """
 
-#导入sys库
+# 导入sys库
 import sys 
-#临时添加本地库（添加库的搜索路径），在列表的任意位置添加目录，新添加的目录会优先于其它目录被import检查
+# 临时添加本地库（添加库的搜索路径），在列表的任意位置添加目录，新添加的目录会优先于其它目录被import检查
 sys.path.insert(0, '../../Utilities/') 
+
+# 调库
