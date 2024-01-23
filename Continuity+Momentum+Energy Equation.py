5e57a9618d50d9ff79ff05f515b3cff9674ed8f5
KyZhangS <142552994+KyzhangPy@users.noreply.github.com>
2024-01-23 15:45:51 +0800

    Update Continuity+Momentum+Energy Equation.py

--- Continuity+Momentum+Energy Equation.py
+++ Continuity+Momentum+Energy Equation.py
@@ -4,17 +4,18 @@
 
 # 导入sys库
 import sys 
-# 临时添加本地库（添加库的搜索路径），在列表的任意位置添加目录，新添加的目录会优先于其它目录被import检查
+# 临时添加本地库（添加import库的搜索路径），在列表的任意位置添加目录，新添加的目录会优先于其它目录被import检查
 sys.path.insert(0, '../../main/') 
 
 # 调库
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.io
-from scipy.interpolate import griddata
-import time
-from itertools import product, combinations
+import tensorflow as tf ##调用tensorflow开发工具
+import numpy as np ##调用科学计算工具 
+import matplotlib.pyplot as plt ##调用python的画图功能库
+import scipy.io ##调用物理常量/单位库、常用的输入输出函数
+from scipy.interpolate import griddata #调用griddata差值函数（非规则网格的数据差值）
+import time ##调用时间日期函数
+from itertools import product, combinations 
+##itertools是用于迭代工具的标准库，itertools.product计算多个可迭代对象的笛卡尔积，itertools.combinations生成可迭代对象的所有长度为r的组合
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from plotting import newfig, savefig
