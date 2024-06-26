d48f2459c79876ab9c5127bb7f186315e3a875ca
KyZhangShuo <142552994+KyzhangPy@users.noreply.github.com>
2024-06-26 17:55:55 +0800

    Update Burgers Equation_Pytorch.py

--- Burgers Equation_Pytorch.py
+++ Burgers Equation_Pytorch.py
@@ -7,9 +7,11 @@ import sys
 # 临时添加本地库（添加import库的搜索路径），在列表的任意位置添加目录，新添加的目录会优先于其它目录被import检查
 sys.path.insert(0, '../Utilities/')## 此处需手动输入位置路径
 
-# 导入库
+# 调库
 import torch
+## 调用Pytorch
 from collections import OrderedDict
+## 调用OrderedDict，OrderedDict是Python标准库中的一个数据结构，是一个有序的字典，可以记住元素的插入顺序，可以按照元素插入的顺序来迭代字典中的键值对
 
 import numpy as np
 import matplotlib.pyplot as plt
