d1f6c49f294e1ddc50911de66308835a83c277a8
KyZhangShuo <142552994+KyzhangPy@users.noreply.github.com>
2024-06-26 17:51:49 +0800

    Update Burgers Equation_Pytorch.py

--- Burgers Equation_Pytorch.py
+++ Burgers Equation_Pytorch.py
@@ -5,8 +5,9 @@
 # 导入sys库
 import sys
 # 临时添加本地库（添加import库的搜索路径），在列表的任意位置添加目录，新添加的目录会优先于其它目录被import检查
-sys.path.insert(0, '../Utilities/')
+sys.path.insert(0, '../Utilities/')## 此处需手动输入位置路径
 
+# 导入库
 import torch
 from collections import OrderedDict
 
