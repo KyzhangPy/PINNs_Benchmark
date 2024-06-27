8ee904ccebc39d931845c2db3bf66174d4bdb840
KyZhangShuo <142552994+KyzhangPy@users.noreply.github.com>
2024-06-27 10:04:52 +0800

    Update Burgers Equation_Pytorch.py

--- Burgers Equation_Pytorch.py
+++ Burgers Equation_Pytorch.py
@@ -14,21 +14,23 @@ from collections import OrderedDict
 ## 调用OrderedDict，OrderedDict是Python标准库中的一个数据结构，是一个有序的字典，可以记住元素的插入顺序，可以按照元素插入的顺序来迭代字典中的键值对
 
 import numpy as np
-## 调用numpy-科学计算工具库
+## 导入numpy-科学计算工具库
 import matplotlib.pyplot as plt
-## 调用pyplot-画图功能库
+## 导入pyplot-画图功能库
 import scipy.io
-## 调用scipy.io-物理常量/单位库、常用的输入输出函数库
+## 导入scipy.io-物理常量/单位库、常用的输入输出函数库
 from scipy.interpolate import griddata
-## 调用griddata-差值函数库（非规则网格的数据差值）
+## 导入griddata-差值函数库（非规则网格的数据差值）
 from plotting import newfig, savefig
-## 调用plotting-可以结合各种视觉元素和工具创建可视化图形的库
+## 导入plotting-可以结合各种视觉元素和工具创建可视化图形的库
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-## 调用axes_grid1-辅助类几何工具，可以用于显示多个图像
+## 导入axes_grid1-辅助类几何工具，可以用于显示多个图像
 import matplotlib.gridspec as gridspec
-
+## 导入gridspec-专门指定画布中的子图位置的模块
 import warnings
-
+## 导入warnings模块-用于处理警告消息，使程序在运行时可捕捉并处理一些非致命性问题，不中断程序的执行，通常用于提醒开发者一些潜在问题或不推荐用法，但不阻止程序继续执行
 warnings.filterwarnings('ignore')
-
+## warnings.filterwarnings('ignore')-运行时忽略掉所有警告
+## warnings.filterwarnings("ignore", category=DeprecationWarning)-忽略特定类型的警告
+## warnings.filterwarnings("ignore", message="some specific warning message")-忽略特定消息的警告
 np.random.seed(1234)
