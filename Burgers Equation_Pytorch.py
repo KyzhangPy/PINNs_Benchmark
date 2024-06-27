1a71967f485578adb4ae3c8dc8cfceb4d8a70597
KyZhangShuo <142552994+KyzhangPy@users.noreply.github.com>
2024-06-27 10:13:53 +0800

    Update Burgers Equation_Pytorch.py

--- Burgers Equation_Pytorch.py
+++ Burgers Equation_Pytorch.py
@@ -2,17 +2,18 @@
 @author: Zhang Shuo
 ###
 
+###
 # 调入sys库
 import sys
 # 临时添加本地库（添加import库的搜索路径），在列表的任意位置添加目录，新添加的目录会优先于其它目录被import检查
 sys.path.insert(0, '../Utilities/')## 此处需手动输入位置路径
 
+###
 # 调库
 import torch
 ## 调用Pytorch
 from collections import OrderedDict
 ## 调用OrderedDict，OrderedDict是Python标准库中的一个数据结构，是一个有序的字典，可以记住元素的插入顺序，可以按照元素插入的顺序来迭代字典中的键值对
-
 import numpy as np
 ## 导入numpy-科学计算工具库
 import matplotlib.pyplot as plt
@@ -27,10 +28,24 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 ## 导入axes_grid1-辅助类几何工具，可以用于显示多个图像
 import matplotlib.gridspec as gridspec
 ## 导入gridspec-专门指定画布中的子图位置的模块
+
+###
+# 导入warnings模块-用于处理警告消息，使程序在运行时可捕捉并处理一些非致命性问题，不中断程序的执行，通常用于提醒开发者一些潜在问题或不推荐用法，但不阻止程序继续执行
 import warnings
-## 导入warnings模块-用于处理警告消息，使程序在运行时可捕捉并处理一些非致命性问题，不中断程序的执行，通常用于提醒开发者一些潜在问题或不推荐用法，但不阻止程序继续执行
 warnings.filterwarnings('ignore')
 ## warnings.filterwarnings('ignore')-运行时忽略掉所有警告
 ## warnings.filterwarnings("ignore", category=DeprecationWarning)-忽略特定类型的警告
 ## warnings.filterwarnings("ignore", message="some specific warning message")-忽略特定消息的警告
+
+###
+# 设置随机数种子
 np.random.seed(1234)
+
+###
+# 设置计算单元CUDA
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+### 
